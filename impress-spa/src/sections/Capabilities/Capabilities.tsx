2bd364f5479623da5675aef8240d4e995a7f68d4
Svetoslav Georgiev <svetoslav.s.georgiev.84@gmail.com>
2026-07-11 23:52:25 +0300

    fix: remove productionImage

--- impress-spa/src/sections/Capabilities/Capabilities.tsx
+++ impress-spa/src/sections/Capabilities/Capabilities.tsx
@@ -1,6 +1,5 @@
 import Container from "../../components/ui/Container/Container";
 import Section from "../../components/ui/Section/Section";
-import productionImage from "../../assets/images/production.webp";
 import { useTranslation } from "react-i18next";
 
 function Capabilities() {
